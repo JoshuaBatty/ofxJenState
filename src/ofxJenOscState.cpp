41b5c1809b958e3634ad713b581e31d60dc88019
mitchmindtree <mitchell.nordine@gmail.com>
2014-11-01 19:15:34 +1100

    Added State methods to return const pointers to playheads, rhythms or rhythms of a specific type in a vector

--- src/ofxJenOscState.cpp
+++ src/ofxJenOscState.cpp
@@ -8,4 +8,54 @@
 
 #include "ofxJenOscState.h"
 
+using namespace ofxJenOsc;
 
+double Playhead::percentage() {
+    if (durationInTicks > 0) {
+        return (double)positionInTicks / (double)durationInTicks;
+    } else {
+        return 0.0;
+    }
+}
+
+void State::update() {
+	while(oscReceiver.hasWaitingMessages()){
+		ofxOscMessage m;
+		oscReceiver.getNextMessage(&m);
+        
+        // Use messages to update current state here...
+        
+    }
+}
+
+inline vector<const Playhead*> State::getPlayheads() {
+    vector<const Playhead*> vec;
+    vec.reserve(playheads.size());
+    for (int i = 0; i < playheads.size(); i++) {
+        const Playhead* playhead = &playheads[i];
+        vec.push_back(playhead);
+    }
+    return vec;
+}
+
+inline vector<const Rhythm*> State::getRhythms() {
+    vector<const Rhythm*> vec;
+    vec.reserve(rhythms.size());
+    for (int i = 0; i < rhythms.size(); i++) {
+        const Rhythm* rhythm = &rhythms[i];
+        vec.push_back(rhythm);
+    }
+    return vec;
+}
+
+inline vector<const Rhythm*> State::getRhythmsOfType(RhythmType type) {
+    vector<const Rhythm*> vec;
+    vec.reserve(rhythms.size());
+    for (int i = 0; i < rhythms.size(); i++) {
+        if (rhythms[i].type == type) {
+            const Rhythm* rhythm = &rhythms[i];
+            vec.push_back(rhythm);
+        }
+    }
+    return vec;
+}
