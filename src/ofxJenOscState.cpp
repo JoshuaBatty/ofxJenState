6a08a1b27fc8cfc0451ed084a2dbb702d2fd3858
mitchmindtree <mitchell.nordine@gmail.com>
2014-11-01 17:27:18 +1100

    Updated mindmap

---
+++ src/ofxJenOscState.cpp
@@ -0,0 +1,9 @@
+//
+//  ofxJenOscState.cpp
+//  example_ofxJenOsc
+//
+//  Created by Mitchell Nordine on 1/11/2014.
+//
+//
+
+#include "ofxJenOscState.h"
---
+++ src/ofxJenOscState.h
@@ -0,0 +1,14 @@
+//
+//  ofxJenOscState.h
+//  example_ofxJenOsc
+//
+//  Created by Mitchell Nordine on 1/11/2014.
+//
+//
+
+#ifndef __example_ofxJenOsc__ofxJenOscState__
+#define __example_ofxJenOsc__ofxJenOscState__
+
+#include <stdio.h>
+
+#endif /* defined(__example_ofxJenOsc__ofxJenOscState__) */
