f69923605d35c073a844627ee37eb1a50e5112f2
Joshua Batty <joshpbatty@gmail.com>
2015-09-11 20:57:43 +1000

    think for now we have finished String parsing of JenState :)

--- src/ofxJenState.cpp
+++ src/ofxJenState.cpp
@@ -11,7 +11,8 @@
 using namespace ofxJenState;
 
 
-float parsePlayhead(string s) {
+float parseFloat(string s) {
+    //cout << "parseFloat s = " << s << endl;
     int semicolon = s.find(":");
     string number = s.substr(semicolon + 2, s.size()-1);
     return ofToFloat(number);
@@ -19,92 +20,281 @@ float parsePlayhead(string s) {
 
 void State::update() {
 
+    // Pull in new messages from OSC
+    osc_receiver.update();
+    
     // Check for new received string.
-    if (!receiver.hasUpdated) {
-        //return;
+    if (!osc_receiver.hasUpdated) {
+        return;
+    } else {
+        osc_receiver.hasUpdated = false;
     }
     
-    string sampleState = "StateView { playhead: PlayheadState { tick_count: 1060530, phrase_perc: 0.9863560267857143, segment_perc: 0.9727120535714285, bar_perc: 0.7816964285714286, minim_perc: 0.490625, beat_perc: 0.7359375, quaver_perc: 0.471875, semi_quaver_perc: 0.94375, time_sig: TimeSig { top: 7, bottom: 8 }, bpm: 131 }, dsp: DspState { instruments: {Melodic: (0.0456732, 0)}, busses: {}, master: (0.1376343, 0) } }";
+    //string stateString = "StateView { playhead: PlayheadState { tick_count: 101060, phrase_perc: 0.3289713541666667, segment_perc: 0.31588541666666664, bar_perc: 0.6317708333333333, minim_perc: 0.6317708333333333, beat_perc: 0.2635416666666667, quaver_perc: 0.5270833333333333, semi_quaver_perc: 0.05416666666666667, time_sig: TimeSig { top: 2, bottom: 4 }, bpm: 85 }, dsp: DspState { instruments: {(Kick, 1): (0.04907299, 0), (Snare, 0): (0.0014198811, 0), (Chordal, 4): (0.028520431, 0), (Bass, 2): (0.249059, 0), (Chordal, 5): (0.02641474, 0), (Melodic, 3): (0.051713094, 0)}, busses: {}, master: (0.1094684, 0) }, note: NoteState { instruments: {(Snare, 0): 0.2635416666666667, (Melodic, 3): 0.5270833333333333, (Chordal, 5): 0.31588541666666664, (Kick, 1): 0.2635416666666667, (Bass, 2): 0.4434375} } }";
     
+    //cout << "osc_receiver.stateString = " << osc_receiver.stateString  << endl;
     
-    cout << sampleState << endl << endl;
+    string stateString = osc_receiver.stateString;
     
-    vector<string> jenStates;
-    
-    int offsets[] = {38,212,53,200};
-    offset_separator f(offsets, offsets+4);
-    tokenizer<offset_separator> tok(sampleState,f);
-    for(tokenizer<offset_separator>::iterator beg=tok.begin(); beg!=tok.end();++beg){
-        cout << *beg << "\n";
-        jenStates.push_back(*beg);
-    }
-    
+    ///--------- PLAY HEADS
     int tick_count_pos = 0;
-    int phrase_perc_pos = jenStates[1].find(", phrase_perc: ");
-    int segment_perc_pos = jenStates[1].find(", segment_perc: ");
-    int bar_perc_pos = jenStates[1].find(", bar_perc: ");
-    int minim_perc_pos = jenStates[1].find(", minim_perc: ");
-    int beat_perc_pos = jenStates[1].find(", beat_perc: ");
-    int quaver_perc_pos = jenStates[1].find(", quaver_perc: ");
-    int semi_quaver_perc_pos = jenStates[1].find(", semi_quaver_perc: ");
-
-    playheads[Phrase] = parsePlayhead(jenStates[1].substr(phrase_perc_pos, segment_perc_pos));
-    playheads[Segment] = parsePlayhead(jenStates[1].substr(segment_perc_pos, bar_perc_pos));
-    playheads[Bar] = parsePlayhead(jenStates[1].substr(bar_perc_pos, minim_perc_pos));
-    playheads[Minim] = parsePlayhead(jenStates[1].substr(minim_perc_pos, beat_perc_pos));
-    playheads[Beat] = parsePlayhead(jenStates[1].substr(beat_perc_pos, quaver_perc_pos));
-    playheads[Quaver] = parsePlayhead(jenStates[1].substr(quaver_perc_pos, semi_quaver_perc_pos));
-    playheads[SemiQuaver] = parsePlayhead(jenStates[1].substr(semi_quaver_perc_pos, jenStates[1].size()-2));
+    int phrase_perc_pos = stateString.find(", phrase_perc: ");
+    int segment_perc_pos = stateString.find(", segment_perc: ");
+    int bar_perc_pos = stateString.find(", bar_perc: ");
+    int minim_perc_pos = stateString.find(", minim_perc: ");
+    int beat_perc_pos = stateString.find(", beat_perc: ");
+    int quaver_perc_pos = stateString.find(", quaver_perc: ");
+    int semi_quaver_perc_pos = stateString.find(", semi_quaver_perc: ");
+    int end_of_playheads_plus_ten = stateString.find(", time_sig");
     
-    for (int i=0; i < TotalMeasures; i++) {
-        cout << i << ": " << playheads[i] << endl;
+    playheads.params[Phrase] = parseFloat(stateString.substr(phrase_perc_pos, segment_perc_pos - phrase_perc_pos));
+    playheads.params[Segment] = parseFloat(stateString.substr(segment_perc_pos, bar_perc_pos - segment_perc_pos));
+    playheads.params[Bar] = parseFloat(stateString.substr(bar_perc_pos, minim_perc_pos - bar_perc_pos));
+    playheads.params[Minim] = parseFloat(stateString.substr(minim_perc_pos, beat_perc_pos - minim_perc_pos));
+    playheads.params[Beat] = parseFloat(stateString.substr(beat_perc_pos, quaver_perc_pos - beat_perc_pos));
+    playheads.params[Quaver] = parseFloat(stateString.substr(quaver_perc_pos, semi_quaver_perc_pos - quaver_perc_pos));
+    playheads.params[SemiQuaver] = parseFloat(stateString.substr(semi_quaver_perc_pos, end_of_playheads_plus_ten - 10 - semi_quaver_perc_pos));
+    
+    ///--------- TIME SIG & BPM
+    int time_sig_top_pos = stateString.find("TimeSig { top: ");
+    int time_sig_bottom_pos = stateString.find(", bottom: ");
+    int bpm_pos = stateString.find(" }, bpm: ");
+    int bpm_end_pos = stateString.find(" }, dsp");
+    
+    timeSigTop = parseFloat(stateString.substr(time_sig_top_pos, time_sig_bottom_pos - time_sig_top_pos));
+    timeSigBottom = parseFloat(stateString.substr(time_sig_bottom_pos, bpm_pos - time_sig_bottom_pos));
+    bpm = parseFloat(stateString.substr(bpm_pos, bpm_end_pos - bpm_pos));
+    
+    ///--------- DSP
+    int dsp_start_pos = stateString.find("DspState");
+    string dspState = stateString.substr(dsp_start_pos, stateString.size()-1 - dsp_start_pos);
+    
+    ///--------- INSTRUMENTS
+    int instrument_start_pos = 25;
+    int instrument_end_pos = dspState.find("}, busses");
+    string instrumentState = dspState.substr(instrument_start_pos, instrument_end_pos - instrument_start_pos);
+    
+    int start_index = 0;
+    while (true) {
+        
+        int end_idx = instrumentState.find("),", start_index);
+        
+        // If we didn't find any more endings, we're done.
+        bool last_inst = false;
+        if (end_idx == -1) {
+            last_inst = true;
+        }
+    
+        string inst = instrumentState.substr(start_index, end_idx - start_index);
+        
+        // If there are no strings in the list, bail.
+        if (inst == "") {
+            break;
+        } else {
+            parseInstrument(inst);
+        }
+        
+        if (last_inst) { break; }
+        start_index = end_idx + 3; // 3 == "), ".size()
     }
-
-  
-//    typedef tokenizer<boost::char_separator<char> > tokenizer;
-//    char_separator<char> sep(",", ",", keep_empty_tokens);
-//    //    char_separator<char> sep(" ", ":,", keep_empty_tokens);
-//    tokenizer tok2(jenStates[1], sep);
-//    
-//    for(tokenizer::iterator beg=tok2.begin(); beg!=tok2.end();++beg){
-//        cout << *beg << "\n";
-//        if(*beg != ","){
-//            
-//            
-//            cout << strs.size() << endl;
-//            
-//            for (size_t i = 0; i < strs.size(); i++)
-//                cout << strs[i] << endl;
-//            //cout << strs[1] << ": " << strs[3] << endl;
-//        }
-//    }
     
-    receiver.hasUpdated = false;
+    ///--------- BUSSES
+    int bus_start_pos = dspState.find("busses") + 9; // Inside the curly.
+    int bus_end_pos = dspState.find("}, master");
+    string busState = dspState.substr(bus_start_pos, bus_end_pos - bus_start_pos);
+
+    
+    ///--------- MASTER
+    int master_rms_start_pos = dspState.find("master") + 9; // Straight to the rms value mayte.
+    int master_rms_end_pos = dspState.find(", ", master_rms_start_pos);
+    string masterRmsString = dspState.substr(master_rms_start_pos, master_rms_end_pos - master_rms_start_pos);
+    master.params[BUS_RMS] = ofToFloat(masterRmsString);
+    
+    ///--------- NOTES
+    int note_start_pos = dspState.find("NoteState") + 26; // Inside the Curly
+    int note_end_pos = dspState.find("}" , note_start_pos);
+    
+    int start_idx = note_start_pos;
+    while (true) {
+        int end_idx = dspState.find(", ", start_idx);
+        
+        bool last_note = false;
+        if(end_idx == -1){
+            last_note = true;
+        }
+        
+        string note = dspState.substr(start_idx, end_idx - start_idx);
+        parseNote(note);
+        
+        if (last_note) { break; }
+        start_idx = end_idx + 2;
+    }
+    
+//    for (int i=0; i < TotalMeasures; i++) {
+//        cout << i << ": " << playheads.params[i] << endl;
+//    }
+//    cout << "Time Sig Top = " << timeSigTop << endl;
+//    cout << "Time Sig Bottom = " << timeSigBottom << endl;
+//    cout << "BPM = " << bpm << endl;
+//    
+//    for(int i = 0; i < instruments.instruments.size(); i++){
+//        cout << "instrument Name = " << instruments.instruments[i].name << endl;
+//        cout << "instrument " + ofToString(1+i) + " Rms = " << instruments.instruments[i].params[RMS] << endl;
+//        cout << "instrument " + ofToString(1+i) + " Pan = " << instruments.instruments[i].params[PAN] << endl;
+//        cout << "instrument " + ofToString(1+i) + " Perc = " << instruments.instruments[i].params[PERC] << endl;
+//        cout << " ---------------------- " << endl;
+//    }
+//    
+//    cout << "MASTER RMS = " << masterRmsString << endl;
+    
+  //  receiver.hasUpdated = false;
 }
 
 //--------------------------------------------------------------
-void Receiver::threadedFunction(){
-    while(isThreadRunning())
-    {
-        // Attempt to lock the mutex. If blocking is turned on,
-        if(lock())
-        {
-            if (!hasUpdated) {
-                nanoPull.receive(stateString);
-                hasUpdated = true;
-            }
-            
-            // Unlock the mutex. This is only
-            // called if lock() returned true above.
-            unlock();
-        }
-        else
-        {
-            // If we reach this else statement, it means that we could not
-            // lock out mutex, and so we do not need to call unlock().
-            // Calling unlock without locking will lead to porblems.
-            ofLogWarning("threadedFunction()") << "Unable to lock mutex.";
+// s is something like "(Snare, 0): 0.2635416666666667"
+void State::parseNote(string s){
+    //cout << "note s = " << s << endl;
+    int perc_pos = s.find(": ") + 2;
+    int perc_end = s.find(s.size()-1);
+    string perc_string = s.substr(perc_pos, perc_end - perc_pos);
+    float perc_val = ofToFloat(perc_string);
+    
+    if (s.find("Kick") != -1) {
+        instruments.instruments[Kick].params[PERC] = perc_val;
+    } else if (s.find("Snare") != -1){
+        instruments.instruments[Snare].params[PERC] = perc_val;
+    } else if (s.find("Ride") != -1){
+        instruments.instruments[Ride].params[PERC] = perc_val;
+    } else if (s.find("Ghost") != -1){
+        instruments.instruments[Ghost].params[PERC] = perc_val;
+    } else if (s.find("Bass") != -1){
+        instruments.instruments[Bass].params[PERC] = perc_val;
+    } else if (s.find("Melodic") != -1){
+        instruments.instruments[Melodic].params[PERC] = perc_val;
+    } else if (s.find("Chordal") != -1){
+        instruments.instruments[Chordal].params[PERC] = perc_val;
+    } else if (s.find("Atmos") != -1){
+        instruments.instruments[Atmos].params[PERC] = perc_val;
+    }
+}
+
+//--------------------------------------------------------------
+// s is something like "(Chordal, 4): (0.028520431, 0)"
+void State::parseInstrument(string s){
+    cout << "inst s = " << s << endl;
+
+    int rms_pos = s.find("(", 2) + 1; // 0.028520431, 0)
+    int first_comma = s.find(",");
+    int rms_end = s.find(",", first_comma + 2);
+    
+    string rms_string = s.substr(rms_pos, rms_end - rms_pos);
+    float rms_val = ofToFloat(rms_string);
+    
+    int pan_pos = rms_end + 2;
+    int pan_end = s.size()-1;
+    
+    string pan_string = s.substr(pan_pos, pan_end - pan_pos);
+    float pan_val = ofToFloat(pan_string);
+    
+    if (s.find("Kick") != -1) {
+        instruments.instruments[Kick].params[RMS] = ofClamp(ofMap(rms_val,0.0,0.6,0.0,1.0), 0.0, 1.0);
+        instruments.instruments[Kick].params[PAN] = pan_val;
+    } else if (s.find("Snare") != -1){
+        instruments.instruments[Snare].params[RMS] = ofClamp(ofMap(rms_val,0.0,0.15,0.0,1.0), 0.0, 1.0);
+        instruments.instruments[Snare].params[PAN] = pan_val;
+    } else if (s.find("Ride") != -1){
+        instruments.instruments[Ride].params[RMS] = ofClamp(ofMap(rms_val,0.0,0.08,0.0,1.0), 0.0, 1.0);
+        instruments.instruments[Ride].params[PAN] = pan_val;
+    } else if (s.find("Ghost") != -1){
+        instruments.instruments[Ghost].params[RMS] = ofClamp(ofMap(rms_val,0.0,0.05,0.0,1.0), 0.0, 1.0);
+        instruments.instruments[Ghost].params[PAN] = pan_val;
+    } else if (s.find("Bass") != -1){
+        instruments.instruments[Bass].params[RMS] = ofClamp(ofMap(rms_val,0.0,0.3,0.0,1.0), 0.0, 1.0);
+        instruments.instruments[Bass].params[PAN] = pan_val;
+    } else if (s.find("Melodic") != -1){
+        instruments.instruments[Melodic].params[RMS] = ofClamp(ofMap(rms_val,0.0,0.1,0.0,1.0), 0.0, 1.0);
+        instruments.instruments[Melodic].params[PAN] = pan_val;
+    } else if (s.find("Chordal") != -1){
+        instruments.instruments[Chordal].params[RMS] = ofClamp(ofMap(rms_val,0.0,0.125,0.0,1.0), 0.0, 1.0);
+        instruments.instruments[Chordal].params[PAN] = pan_val;
+    } else if (s.find("Atmos") != -1){
+        instruments.instruments[Atmos].params[RMS] = ofClamp(ofMap(rms_val,0.0,0.1,0.0,1.0), 0.0, 1.0);
+        instruments.instruments[Atmos].params[PAN] = pan_val;
+    }
+}
+
+//--------------------------------------------------------------
+//--------------------------------------------------------------
+vector<Modulator*> State::getChildren() {
+    vector<Modulator*> children;
+    children.reserve(5);
+    
+    children.push_back((Modulator*)&instruments);
+    children.push_back((Modulator*)&playheads);
+    children.push_back((Modulator*)&busses);
+    children.push_back((Modulator*)&master);
+    
+    return children;
+}
+
+vector<Modulator*> Instruments::getChildren(){
+    vector<Modulator*> children;
+    children.reserve(instruments.size());
+    
+    for (int i=0; i < instruments.size(); i++) {
+        children.push_back((Modulator*)&instruments[i]);
+    }
+    
+    return children;
+}
+
+vector<Modulator*> Busses::getChildren(){
+    vector<Modulator*> children;
+    children.reserve(busses.size());
+    
+    for (int i=0; i < busses.size(); i++) {
+        children.push_back((Modulator*)&busses[i]);
+    }
+    
+    return children;
+}
+
+//--------------------------------------------------------------
+void OSC_Receiver::update(){
+    while (receiver.hasWaitingMessages()) {
+        ofxOscMessage m;
+        receiver.getNextMessage(&m);
+        
+        // TRIGGERS
+        if(m.getAddress() == "/jenState"){
+            stateString = m.getArgAsString(0);
+            hasUpdated = true;
         }
     }
 }
+////--------------------------------------------------------------
+//void Receiver::threadedFunction(){
+//    while(isThreadRunning())
+//    {
+//        // Attempt to lock the mutex. If blocking is turned on,
+//        if(lock())
+//        {
+//            if (!hasUpdated) {
+//                //nanoPull.receive(stateString);
+//                hasUpdated = true;
+//            }
+//            
+//            // Unlock the mutex. This is only
+//            // called if lock() returned true above.
+//            unlock();
+//        }
+//        else
+//        {
+//            // If we reach this else statement, it means that we could not
+//            // lock out mutex, and so we do not need to call unlock().
+//            // Calling unlock without locking will lead to porblems.
+//            ofLogWarning("threadedFunction()") << "Unable to lock mutex.";
+//        }
+//    }
+//}
 
--- src/ofxJenState.h
+++ src/ofxJenState.h
@@ -9,12 +9,10 @@
 #pragma once
 
 #include "ofMain.h"
-#include "ofThread.h"
-#include "ofxNanomsg.h"
-#include <boost/tokenizer.hpp>
-#include <boost/algorithm/string.hpp>
-
-using namespace boost;
+//#include "ofThread.h"
+//#include "ofxNanomsg.h"
+#include "ofxOsc.h"
+#include "Modulator.h"
 
 namespace ofxJenState {
 
@@ -29,6 +27,15 @@ namespace ofxJenState {
         Quaver,
         SemiQuaver,
         TotalMeasures
+    };
+    
+    class Playheads : public Modulator{
+    public:
+        Playheads(){
+            for(int i = 0; i < TotalMeasures; i++){
+                params.push_back(0.0);
+            }
+        }
     };
 
 
@@ -61,47 +68,123 @@ namespace ofxJenState {
         Atmos
     };
     
+    
+    enum InstrumentParam{
+        RMS,
+        PERC,
+        PAN,
+        TotalInstrumentParams
+    };
+    
     /// Represents the state of an Instrument.
-    class Instrument {
+    class Instrument: public Modulator {
     public:
 
-        Instrument(PatternType _type, string _name, vector<float> _notePlayheads)
-        : type(_type)
-        , name(_name)
-        , notePlayheads(_notePlayheads) {
+        Instrument(PatternType _type)
+        : type(_type) {
+            switch (_type) {
+                case Kick: name = "kick"; break;
+                case Snare: name = "Snare"; break;
+                case Ghost: name = "Ghost"; break;
+                case Ride: name = "Ride"; break;
+                case Layer: name = "Layer"; break;
+                case Bass: name = "Bass"; break;
+                case Melodic: name = "Melodic"; break;
+                case Chordal: name = "Chordal"; break;
+                case Atmos: name = "Atmos"; break;
+                    
+                default:
+                    break;
+            }
+            
+            for(int i = 0; i < TotalInstrumentParams; i++){
+                params.push_back(0.0);
+            }
         }
 
-        const PatternType type;
-        const string name;
-        vector<float> notePlayheads;
-        float amp;
-        float pan; /// -1.0 is far left, 0.0 is center and 1.0 is far right.
-
-    };
-    
-    class Bus {
-    public:
-        
-        Bus(string _name, float _vol)
-        : name(_name)
-        , vol(_vol) {
-        }
-        
+        PatternType type;
         string name;
-        float vol;
     };
     
-    
-    class Receiver: public ofThread {
-        
+    class Instruments: public Modulator {
     public:
-        Receiver() {
-            nanoPull.bind("tcp://*:9000");
+        Instruments()
+        {
+            instruments.reserve(8);
+            instruments.push_back(Instrument(Kick));
+            instruments.push_back(Instrument(Snare));
+            instruments.push_back(Instrument(Ghost));
+            instruments.push_back(Instrument(Ride));
+            instruments.push_back(Instrument(Layer));
+            instruments.push_back(Instrument(Bass));
+            instruments.push_back(Instrument(Melodic));
+            instruments.push_back(Instrument(Chordal));
+            instruments.push_back(Instrument(Atmos));
+            
         }
-        
-        void threadedFunction();
-        
-        ofxNanomsgPull nanoPull;
+        vector<Instrument> instruments;
+        vector<Modulator*> getChildren();
+    };
+    
+    enum BusParams{
+        BUS_RMS,
+        BUS_PAN,
+        TotalBusParams
+    };
+    
+    class Bus : public Modulator {
+    public:
+        Bus(string _name)
+        : name(_name)
+        {
+            for(int i = 0; i < TotalBusParams; i++){
+                params.push_back(0.0);
+            }
+        }
+        string name;
+    };
+    
+    class Busses: public Modulator{
+    public:
+        Busses()
+        {
+            busses.reserve(5);
+            busses.push_back(Bus("Bus 1"));
+            busses.push_back(Bus("Bus 2"));
+            busses.push_back(Bus("Bus 3"));
+            busses.push_back(Bus("Bus 4"));
+            busses.push_back(Bus("Bus 5"));
+        }
+        vector<Bus> busses;
+        vector<Modulator*> getChildren();
+    };
+    
+//    class Receiver: public ofThread {
+//        
+//    public:
+//        Receiver() {
+//            nanoPull.bind("tcp://*:9001");
+//        }
+//        
+//        void threadedFunction();
+//        
+//        ofxNanomsgPull nanoPull;
+//        bool hasUpdated;
+//    };
+
+    
+    
+    class OSC_Receiver {
+    public:
+        OSC_Receiver(){
+            OSC_PORT = 9001;
+            receiver.setup(OSC_PORT);
+            stateString = "";
+            hasUpdated = false;
+        }
+        void update();
+        ofxOscReceiver receiver;
+        int OSC_PORT;
         string stateString;
         bool hasUpdated;
     };
@@ -109,34 +192,37 @@ namespace ofxJenState {
 
     /// Represents the entire state of Jen. By calling `update`, the state will
     /// check for OSC messages and update its entire state accordingly.
-    class State {
+    class State: public Modulator {
     public:
 
         ~State(){
-            receiver.stopThread();
-            while (receiver.isThreadRunning()) { sleep(10); };
+            //receiver.stopThread();
+            //while (receiver.isThreadRunning());//sleep(10); };
         }
         
         State()
-        : playheads(TotalMeasures, 0.0)
-        , master("MASTER", 0.0) {
-            
-            receiver.startThread();
-            
+        : master("MASTER")
+        {
+            //receiver.startThread();
         }
         
         void update();
+        void parseInstrument(string s);
+        void parseNote(string s);
+        vector<Modulator*> getChildren();
 
-        vector<float> playheads;
-        vector<Instrument> instruments;
-        vector<Bus> busses;
+        Instruments instruments;
+        Playheads playheads;
+        Busses busses;
         Bus master;
+        //Receiver receiver;
+        OSC_Receiver osc_receiver;
         
-        Receiver receiver;
+        int timeSigTop;
+        int timeSigBottom;
+        int bpm;
 
     };
-    
-
 
 }
 
